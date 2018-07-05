7bb3a31ab234cd05c894c80f0aad75482d15b3f8
Jascha Ehrenreich <jascha@jaeh.at>
2018-07-05 18:27:35 +0200

    fix error in option label

--- includes/admin/dashboard.php
+++ includes/admin/dashboard.php
@@ -8,7 +8,7 @@ add_action( 'admin_menu', function () {
       'name' => 'magic_redirect_author_hide',
       'type' => 'checkbox',
       'default' => false,
-      'label' => 'Hide all attachment pages',
+      'label' => 'Hide all author pages',
     ),
     array(
       'name' => 'magic_redirect_author_redirect',
@@ -29,7 +29,7 @@ add_action( 'admin_menu', function () {
       'label' => 'Redirect attachment pages to',
     ),
   );
-  
+
   magic_dashboard_add_submenu_page( array (
     'link' => 'Redirects',
     'slug' => MAGIC_REDIRECT_SLUG,
